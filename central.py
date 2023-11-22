636a4a18469d00c6b0a6d0698a1a3a6ac9c882ef
Ivan <149861268+boosixnew@users.noreply.github.com>
2023-11-22 16:28:38 +0100

    refactor

---
+++ central.py
@@ -0,0 +1,101 @@
+.. image:: https://img.shields.io/github/license/saltstack/salt
+   :target: https://github.com/saltstack/salt/blob/master/LICENSE
+
+   :alt: PyPi Package Downloads
+   :target: https://pypi.org/project/salt
+   :alt: PyPi Package Downloads
+   :target: https://lgtm.com/projects/g/saltstack/salt/context:python
+   :target: https://via.vmw.com/salt-slack
+   :alt: Salt Project Twitch Channel
+   :target: https://www.twitch.tv/saltprojectoss
+
+.. image:: https://img.shields.io/reddit/subreddit-subscribers/saltstack?style=social
+   :alt: Salt Project subreddit
+.. image:: https://img.shields.io/twitter/follow/Salt_Project_OS?style=social&logo=twitter
+   :alt: Follow SaltStack on Twitter
+
+.. figure:: https://gitlab.com/saltstack/open/salt-branding-guide/-/raw/master/logos/SaltProject_altlogo_teal.png?inline=true
+   :scale: 80 %
+   :width: 1000px
+   :height: 356px
+*engine.*
+About Salt
+==========
+deploy, configure, and manage complex IT systems. Use Salt to automate common
+infrastructure are operating in a consistent desired state.
+Salt has many possible uses, including configuration management, which involves:
+
+* Installing and configuring software applications and services.
+* Managing servers, virtual machines, containers, databases, web servers,
+* Ensuring consistent configuration and preventing configuration drift.
+customizable, and plays well with many existing technologies. Salt enables you
+`operating system <https://docs.saltproject.io/salt/install-guide/en/latest/topics/salt-supported-operating-systems.html>`_,
+variety of vendors.
+In addition to configuration management Salt can also:
+
+* Automate and orchestrate routine IT processes, such as common required tasks
+* Create self-aware, self-healing systems that can automatically respond to
+  outages, common administration problems, or other important events.
+
+
+==================
+(previously vRealize Automation SaltStack Config / SaltStack Enterprise), and can be found
+Tieto, to name a few.
+The Salt Project remains an open source ecosystem that VMware supports and
+contributes to. VMware ensures the code integrity and quality of the Salt
+carefully reviews and enhances the Salt modules to ensure speed, quality, and
+
+Download and install Salt
+for more information.
+
+* `The Salt install guide <https://docs.saltproject.io/salt/install-guide/en/latest/index.html>`_
+* `Salt Project repository <https://repo.saltproject.io/>`_
+
+
+Technical support
+
+To join our community forum where you can exchange ideas, best practices,
+discuss technical support questions, and talk to project maintainers, join our
+
+
+Installation instructions, tutorials, in-depth API and module documentation:
+* `The Salt install guide <https://docs.saltproject.io/salt/install-guide/en/latest/index.html>`_
+* `The Salt user guide <https://docs.saltproject.io/salt/user-guide/en/latest/>`_
+* `Latest Salt documentation`_
+
+Keep an eye on the Salt Project
+`Security Announcements <https://saltproject.io/security-announcements/>`_
+announcements.
+
+and the `Salt Project Community Slack`_.
+++++++++++++++++++++++++++++++++++++++++++++++
+refer to the `SECURITY.md`_ file found in this repository.
+
+
+Join our community
+==================
+Salt is built by the Salt Project community, which includes more than 3,000
+creating a variety of execution and state modules to accomplish the most common
+tasks or solve the most important problems that people in your role are likely
+
+If you want to help extend Salt or solve a problem with Salt, you can join our
+community and contribute today.
+`Code of Conduct <https://github.com/saltstack/salt/blob/master/CODE_OF_CONDUCT.md>`_.
+Also, check out some of our community resources including:
+* `Salt Project Community Slack`_
+* `Salt Project YouTube channel <https://www.youtube.com/channel/UCpveTIucFx9ljGelW63-BWg>`_
+* `Salt Project Twitch channel <https://www.twitch.tv/saltprojectoss>`_
+around a dozen opportunities to meet with other contributors and the Salt Core
+to the **Salt Project Community Events Calendar** on the main
+
+directly to the Community Manager, Jimmy Chunga via Slack. We'd be glad to
+
+
+=======
+Salt is licensed under the Apache 2.0 license. Please
+see the
+`LICENSE file <https://github.com/saltstack/salt/blob/master/LICENSE>`_ for the
+
+`salt/DEPENDENCIES.md <https://github.com/saltstack/salt/blob/master/DEPENDENCIES.md>`_
+
+.. _Open an issue: https://github.com/saltstack/salt/issues/new/choose
